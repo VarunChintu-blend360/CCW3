306e5520ba7be7bba52dca86302098e12b0bd686
VarunChintu-blend360 <152614437+VarunChintu-blend360@users.noreply.github.com>
2023-12-03 13:28:03 +0000

    commit

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,5 +1,7 @@
-import streamlit as st
+!pip install snowflake-connector-python
 
+import streamlit as st
+import pandas as pd
 import snowflake.connector
 
 # Snowflake connection parameters
@@ -8,26 +10,40 @@ user = 'varun'
 password = 'Blend360'
 warehouse = 'CCW3_WH'
 database = 'CCW3_DB'
+table_name = 'books_table'
 
-# Snowflake connection
-conn = snowflake.connector.connect(
-    user=user,
-    password=password,
-    account=account
-)
-# Snowflake cursor
-cur = conn.cursor()
-# Use the warehouse
-cur.execute(f"USE WAREHOUSE {warehouse}")
+def fetch_data_from_snowflake():
+    try:
+        # Snowflake connection
+        conn = snowflake.connector.connect(
+            user=user,
+            password=password,
+            account=account
+        )
+        # Snowflake cursor
+        cur = conn.cursor()
+        # Use the warehouse
+        cur.execute(f"USE WAREHOUSE {warehouse}")
 
-# Use the database
-cur.execute(f"USE DATABASE {database}")
-
+        # Use the database
+        cur.execute(f"USE DATABASE {database}")
+        cur.execute(f"SELECT Title, Price, Rating, Availability FROM {table_name}")
+        data = cur.fetchall()
+        columns = [x[0] for x in cur.description]
+        df = pd.DataFrame(data, columns=columns)
+        return df
+    except Exception as e:
+        st.error(f"Error fetching data: {e}")
+    finally:
+        conn.close()
+df = fetch_data_from_snowflake()
 
 # set page configarations
 st.set_page_config(page_title="Coding Challenge 3",layout="wide")
 
-# Header section
-st.header("Coding Challenge 3")
+st.title('Coding Challenge 3')
 st.subheader("Varun Rao Chintu")
 
+
+st.write('Data from Snowflake:')
+st.write(df)
