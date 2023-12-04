9da96a23e7a9cb2a94df81f6d92f771037b0829a
VarunChintu-blend360 <152614437+VarunChintu-blend360@users.noreply.github.com>
2023-12-04 05:08:07 +0000

    Updated

--- streamlit-app.py
+++ streamlit-app.py
@@ -14,6 +14,12 @@ warehouse = 'CCW3_WH'
 database = 'CCW3_DB'
 table_name = 'books_table'
 
+def map_availability(value):
+    if value == True:
+        return 'In Stock'
+    else:
+        return 'Out of Stock'
+
 def fetch_data_from_snowflake():
     try:
         # Snowflake connection
@@ -50,3 +56,27 @@ st.subheader("Varun Rao Chintu")
 st.write('Data from Snowflake:')
 st.dataframe(df)
 
+df['AVAILABILITY'] = df['AVAILABILITY'].apply(map_availability)
+
+
+col1, col2 = st.columns(2)
+with col1:
+    # Create a pie chart for the distribution of ratings
+    rating_counts = df['RATING'].value_counts()
+    fig, ax = plt.subplots()
+    ax.pie(rating_counts, labels=rating_counts.index, autopct='%1.1f%%')
+    ax.axis('equal')
+    st.subheader('Rating Distribution')
+    st.pyplot(fig)
+with col2:
+    # Create a pie chart for the distribution of ratings
+    availability_counts  = df['AVAILABILITY'].value_counts()
+    fig, ax = plt.subplots()
+    ax.pie(availability_counts , labels=availability_counts.index, autopct='%1.1f%%')
+    ax.axis('equal')
+    st.subheader('AVAILABILITY Distribution')
+    st.pyplot(fig)
+
+rated_5_books = df[df['RATING'] == 5]
+st.subheader('Books with Rating 5')
+st.dataframe(rated_5_books[['TITLE', 'PRICE', 'RATING', 'AVAILABILITY']])
